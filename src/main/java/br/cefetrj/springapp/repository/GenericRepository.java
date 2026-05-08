36868129fbce274bf33a8eec67eef37f678a3a19
Vitor Padilha <vitorpadilhagoncalves@gmail.com>
2026-05-07 21:13:57 -0300

    Atualizacao

--- src/main/java/br/cefetrj/springapp/repository/GenericRepository.java
+++ src/main/java/br/cefetrj/springapp/repository/GenericRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import br.cefetrj.springapp.model.EntidadeGenerica;
 
 public interface GenericRepository<T extends EntidadeGenerica>
-        extends CrudRepository<T, Long>, PagingAndSortingRepository<T, Long> {
+                extends CrudRepository<T, Long>, PagingAndSortingRepository<T, Long> {
 
 }
